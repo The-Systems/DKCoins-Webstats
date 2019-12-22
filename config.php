6f150dbbb4f6a743903b71e9b4136c798bb9018a
Niekold <moritz.walter111@gmx.de>
2019-12-22 11:15:38 +0100

    new versionchecker

---
+++ config.php
@@ -0,0 +1,53 @@
+<?php
+/*
+-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*-**-*-*-*
+	CloudNet Webinterface by Niekold (Niekold#9410)
+
+	- You are not allowed to resell the plugin/website
+	- You are not allowed to reupload the plugin/website anywhere else
+    - Refunds are not accepted
+    - any error/bug should be posted in the resource's thread, not in the review section otherwise I will not give a support for reported bugs in     review section
+	- You are not allowed to share this resource with others
+    - You are not allowed to claim ownership of this resource
+
+	Copyrighted by Niekold © 2018
+	
+
+	|-----------|
+	|0 = false	|
+	|1 = true	|
+	|-----------|
+	
+	
+-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-**-*-*-**-*-*-*
+*/
+
+
+/* Configurations */
+
+$domain = "https://nevercold.eu/top-coins";
+$maindomain = "https://nevercold.eu/";
+
+
+$ip = "";
+$user = "";
+$password = "";
+$table = "DKCoins_players";
+$database = "";
+
+$topplayeramount = "10";
+
+$servername = "Nevercold.eu";
+$richname = "Nevercold.eu - Stats";
+$richtext = "Seh dir deine Stats an!";
+
+
+
+$debug = "false";
+/* Dont Change! */
+$version = "1.1";
+$versioncheck = "https://spigot.nevercold.eu/dkcoins/webstats/version.json";
+$true = "true";
+$false = "false";
+include("mysqlclass.php");
+/* Dont Change! */
